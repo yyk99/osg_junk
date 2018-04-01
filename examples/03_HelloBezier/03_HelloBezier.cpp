8a064ff89260845da7e89f5e4dba313937bd0f83
Yuri Kuznetsov <yyktomtom@gmail.com>
2018-03-31 20:59:22 -0500

    Get ready for migration

--- examples/03_HelloBezier/03_HelloBezier.cpp
+++ examples/03_HelloBezier/03_HelloBezier.cpp
@@ -72,13 +72,171 @@ osg::Geode* createTerrain()
     return geode;
 }
 
+//%
+//%
+//%
+//tx = ty = linspace(-8, 8, 41)';
+//[xx, yy] = meshgrid(tx, ty);
+//r = sqrt(xx . ^ 2 + yy . ^ 2) + eps;
+//tz = sin(r) . / r;
+//mesh(tx, ty, tz);
+//xlabel("tx");
+//ylabel("ty");
+//zlabel("tz");
+//title("3-D Sombrero plot");
+
+osg::Geode *createSombreroHat()
+{
+    osg::Geode* geode = new osg::Geode();
+
+    // ---------------------------------------
+    // Set up a StateSet to texture the objects
+    // ---------------------------------------
+    osg::StateSet* stateset = new osg::StateSet();
+
+    osg::ref_ptr<osg::Image> image = osgDB::readRefImageFile("Images/lz.rgb");
+    if (image)
+    {
+        osg::Texture2D* texture = new osg::Texture2D;
+        texture->setImage(image);
+        texture->setFilter(osg::Texture::MIN_FILTER, osg::Texture::LINEAR);
+        stateset->setTextureAttributeAndModes(0, texture, osg::StateAttribute::ON);
+    }
+
+    stateset->setMode(GL_LIGHTING, osg::StateAttribute::ON);
+
+    geode->setStateSet(stateset);
+
+    osg::HeightField* grid = new osg::HeightField;
+    const int N = 41;
+
+    grid->allocate(N, N);
+
+    float dx = 16.0 / (N - 1);
+    float dy = 16.0 / (N - 1);
+
+    grid->setXInterval(dx);
+    grid->setYInterval(dy);
+    //grid->setOrigin(osg::Vec3f(-8, -8, 0));
+
+    float eps = 2.22e-16;
+    for (int r = 0; r < N; ++r) {
+        float y = dy * (r - N/2);
+        for (int c = 0; c < N; ++c) {
+            float x = dx * (c - N/2);
+            float R = sqrt(x *x  + y * y) + eps;
+            float tz = 5 * sin(R) / R;
+            grid->setHeight(c, r, tz);
+
+//            std::cout << tz << " ";
+        }
+    }
+//    std::cout << std::endl;
+    geode->addDrawable(new osg::ShapeDrawable(grid));
+
+    return geode;
+}
+
+#if 0
+//
+//
+//
+function plot3_b3()
+P = zeros(2, 4, 3);
+P(1, :, : ) = [0, 0, 0; 0, 0, 10; 100, 0, -35; 100, 0, 0];
+P(2, :, : ) = [0, 0, 0; 0, 100, 150; 100, 100, 150; 100, 100, 0];
+P(3, :, : ) = [0, 200, 0; 0, 200, 100; 100, 200, 100; 100, 200, 0];
+
+%disp(P);
+
+n = 50;
+X = zeros(1, (n + 1)*(n + 1));
+Y = zeros(size(X));
+Z = zeros(size(X));
+
+tic;
+k = 0;
+for i = 1:n + 1
+u = 1.0 / n * (i - 1);
+for j = 1:n + 1
+k = k + 1;
+v = 1.0 / n * (j - 1);
+B = deCasteljau2(u, v, P);
+X(k) = B(1);
+Y(k) = B(2);
+Z(k) = B(3);
+endfor
+endfor
+toc;
+
+[xx, yy] = meshgrid(linspace(0, 100, n + 1), linspace(0, 200, n + 1));
+zz = griddata(X, Y, Z, xx, yy);
+
+%plot3(X, Y, Z, 'o');
+mesh(xx, yy, zz);
+xlabel("X");
+ylabel("Y");
+zlabel("Z");
+title("3-D plot");
+
+
+endfunction
+
+#endif
+
+//
+osg::Geode *plot3_b3()
+{
+#if 0
+    P = zeros(2, 4, 3);
+    P(1, :, : ) = [0, 0, 0; 0, 0, 10; 100, 0, -35; 100, 0, 0];
+    P(2, :, : ) = [0, 0, 0; 0, 100, 150; 100, 100, 150; 100, 100, 0];
+    P(3, :, : ) = [0, 200, 0; 0, 200, 100; 100, 200, 100; 100, 200, 0];
+
+    %disp(P);
+
+    n = 50;
+    X = zeros(1, (n + 1)*(n + 1));
+    Y = zeros(size(X));
+    Z = zeros(size(X));
+
+    tic;
+    k = 0;
+    for i = 1:n + 1
+        u = 1.0 / n * (i - 1);
+    for j = 1:n + 1
+        k = k + 1;
+    v = 1.0 / n * (j - 1);
+    B = deCasteljau2(u, v, P);
+    X(k) = B(1);
+    Y(k) = B(2);
+    Z(k) = B(3);
+    endfor
+        endfor
+        toc;
+
+    [xx, yy] = meshgrid(linspace(0, 100, n + 1), linspace(0, 200, n + 1));
+    zz = griddata(X, Y, Z, xx, yy);
+
+    %plot3(X, Y, Z, 'o');
+    mesh(xx, yy, zz);
+    xlabel("X");
+    ylabel("Y");
+    zlabel("Z");
+    title("3-D plot");
+#endif
+    return 0;
+}
+
+
+
 int main(int, char **)
 {
     // construct the viewer.
     osgViewer::Viewer viewer;
 
     // add model to viewer.
-    viewer.setSceneData(createTerrain());
+    viewer.setSceneData(createSombreroHat());
 
     return viewer.run();
 }
